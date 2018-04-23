25c4344bcfdd32aa48acdc13cb86e3e4b0a01814
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxScheduler;
+using DevExpress.XtraScheduler;
+
+public partial class Default : System.Web.UI.Page {
+    ASPxSchedulerStorage Storage { get { return ASPxScheduler1.Storage; } }
+
+    protected void Page_Load(object sender, EventArgs e) {
+        SetupMappings();
+        ResourceFiller.FillResources(this.ASPxScheduler1.Storage, 3);
+
+        ASPxScheduler1.AppointmentDataSource = appointmentDataSource;
+        ASPxScheduler1.DataBind();
+
+        ASPxScheduler1.GroupType = SchedulerGroupType.Resource;
+    }
+
+    public string GetResourceColor(ResourceHeaderTemplateContainer container) {
+        int id = ASPxScheduler1.Storage.Resources.Items.IndexOf(container.Resource);
+
+        return ColorTranslator.ToHtml(ASPxScheduler1.ResourceColorSchemas[id % ASPxScheduler1.ResourceColorSchemas.Count].Cell);
+    }
+
+    public string GetResourceHeight() {
+        return ASPxScheduler1.TimelineView.Styles.TimelineCellBody.Height.ToString();
+    }
+
+    void SetupMappings() {
+        ASPxAppointmentMappingInfo mappings = Storage.Appointments.Mappings;
+        Storage.BeginUpdate();
+        try {
+            mappings.AppointmentId = "Id";
+            mappings.Start = "StartTime";
+            mappings.End = "EndTime";
+            mappings.Subject = "Subject";
+            mappings.AllDay = "AllDay";
+            mappings.Description = "Description";
+            mappings.Label = "Label";
+            mappings.Location = "Location";
+            mappings.RecurrenceInfo = "RecurrenceInfo";
+            mappings.ReminderInfo = "ReminderInfo";
+            mappings.ResourceId = "OwnerId";
+            mappings.Status = "Status";
+            mappings.Type = "EventType";
+        }
+        finally {
+            Storage.EndUpdate();
+        }
+    }
+    //Initilazing ObjectDataSource
+    protected void appointmentsDataSource_ObjectCreated(object sender, ObjectDataSourceEventArgs e) {
+        e.ObjectInstance = new CustomEventDataSource(GetCustomEvents());
+    }
+    CustomEventList GetCustomEvents() {
+        CustomEventList events = Session["ListBoundModeObjects"] as CustomEventList;
+        if (events == null) {
+            events = new CustomEventList();
+            Session["ListBoundModeObjects"] = events;
+        }
+        return events;
+    }
+
+    // User generated appointment id    
+    protected void ASPxScheduler1_AppointmentInserting(object sender, PersistentObjectCancelEventArgs e) {
+        SetAppointmentId(sender, e);
+    }
+
+    void SetAppointmentId(object sender, PersistentObjectCancelEventArgs e) {
+        ASPxSchedulerStorage storage = (ASPxSchedulerStorage)sender;
+        Appointment apt = (Appointment)e.Object;
+        storage.SetAppointmentId(apt, apt.GetHashCode());
+    }
+}
